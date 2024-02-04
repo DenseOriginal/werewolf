168752842510cd3fd822b682974ea6daab74e63e
Anders Kok <ak.kornerup@gmail.com>
2024-02-04 01:13:42 +0100

    Add prod sw url

--- src/services/ws.ts
+++ src/services/ws.ts
@@ -1,8 +1,10 @@
-const target = false ?
+const target = import.meta.env.PROD ?
 	"wss://frosted-garrulous-decision.glitch.me:" :
 	`ws://${window.location.hostname}:3000`;
 
 export const getWsClient = () => {
+	console.log(target);
+	
 	const ws = new WebSocket(target);
 
 	ws.onerror = (error) => console.error("WebSocket error", error); 
