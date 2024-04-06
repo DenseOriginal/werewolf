7a8adea9bef5665f684ac51fb9090b28e2cc1d11
Anders Kok <ak.kornerup@gmail.com>
2024-04-06 17:02:25 +0200

    Host: Show delt cards

--- src/views/host/playing.tsx
+++ src/views/host/playing.tsx
@@ -1,8 +1,14 @@
 import { Button } from "@/components/button"
-import { useDispatch } from "@/store";
+import { Hr } from "@/components/hr";
+import { useDispatch, useSelector } from "@/store";
 import { resetGame } from "@/store/host/thunks";
 
 export const PlayingView = () => {
+	const dealtCards = useSelector(state => state.host.game.userCards)
+	const users = useSelector(state => state.host.users);
+
+	const getUsername = (userId: string) => users.find(user => user.userId == userId)?.name;
+
 	const dispatch = useDispatch();
 	
 	const onReset = () => {
@@ -11,6 +17,17 @@ export const PlayingView = () => {
 	
 	return <div className="flex flex-col h-full items-stretch">
 		<span>Playing</span>
+
+		<div className="mt-3">
+			<Hr leftAligned><p className="text-lg">Cards</p></Hr>
+			<ul>
+				{Object.entries(dealtCards).map(entry => (
+					<li>
+						{getUsername(entry[0]) || 'Uknown'} - {entry[1]}
+					</li>
+				))}
+			</ul>
+		</div>
 
 		<Button
 			className="w-full mt-auto mb-9"
